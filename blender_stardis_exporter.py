ddde4579ee148166f5d5302afc652e9db51a869d
Desmond Liu <desmondzyliu@gmail.com>
2024-10-02 07:35:19 +0200

    property panel works properly

--- blender_stardis_exporter.py
+++ blender_stardis_exporter.py
@@ -60,6 +60,28 @@ class ExportCustomFormatOperator(bpy.types.Operator):
 		# Open file browser to choose where to save the file
 		context.window_manager.fileselect_add(self)
 		return {'RUNNING_MODAL'}
+	
+
+class AddStardisObjectProperty(bpy.types.Operator):
+	bl_idname = "object.add_stardis_object_property"
+	bl_label = "Add Stardis Object Property"
+
+	def execute(self, context):
+		obj = context.object
+		obj.stardis_object_properties.add()
+		return {'FINISHED'}
+	
+
+class RemoveStardisObjectProperty(bpy.types.Operator):
+	bl_idname = "object.remove_stardis_object_property"
+	bl_label = "Remove Stardis Object Property"
+
+	index: bpy.props.IntProperty()
+
+	def execute(self, context):
+		obj = context.object
+		obj.stardis_object_properties.remove(self.index)
+		return {'FINISHED'}
 
 
 # Define the enum items
@@ -71,9 +93,9 @@ property_items = [
 ]
 
 # Main Panel: Custom Object Settings
-class OBJECT_PT_main_panel(bpy.types.Panel):
-	bl_label = "Custom Object Settings"
-	bl_idname = "OBJECT_PT_main_panel"
+class StardisObjectPropertyPanel(bpy.types.Panel):
+	bl_label = "Stardis Object Properties"
+	bl_idname = "StardisObjectPropertyPanel"
 	bl_space_type = 'PROPERTIES'
 	bl_region_type = 'WINDOW'
 	bl_context = "object"
@@ -82,111 +104,233 @@ class OBJECT_PT_main_panel(bpy.types.Panel):
 		layout = self.layout
 		obj = context.object
 
-		# Enum to select which group of properties is active
-		layout.prop(obj, "selected_property_group", text="Type")
+		# display the number of properties
+		row = layout.row()
+		row.label(text="Number of Properties: " + str(len(obj.stardis_object_properties)))
 
-		# Conditionally show properties based on the selected group
-		if obj.selected_property_group == 'SOLID':
-			# Display transform properties
-			layout.prop(obj, "conductivity", text="conductivity")
-			layout.prop(obj, "rho", text="rho")
-			layout.prop(obj, "capacity", text="capacity")
-			layout.prop(obj, "delta", text="delta")
-			layout.prop(obj, "initial_temp", text="initial_temp")
-			layout.prop(obj, "imposed_temp", text="imposed_temp")
-			layout.prop(obj, "volumic_power", text="volumic_power")
-		elif obj.selected_property_group == 'DIRICHLET':
-			# Display custom material properties
-			layout.prop(obj, "temp", text="Temperature")
+		# Add a button to add a new property
+		row = layout.row()
+		row.operator("object.add_stardis_object_property", text="Add Stardis Object Property")
 
-		elif obj.selected_property_group == 'ROBIN_SOLID' or obj.selected_property_group == 'ROBIN_FLUID':
-			layout.prop(obj, "emissivity", text="Emissivity")
-			layout.prop(obj, "specular_fraction", text="Specular Fraction")
-			layout.prop(obj, "hc", text="hc")
-			layout.prop(obj, "outside_temp", text="Outside Temp")
+		# Iterate through the properties and display them one by one
+		for i, prop in enumerate(obj.stardis_object_properties):
+			# create a subpanel for each property
+			panel = layout.box()
+			row = panel.row()
+			row.prop(prop, "stardis_object_type", text="Type")
 
-		else:
-			layout.label(text="No properties available for this group")
+			row.operator("object.remove_stardis_object_property", text="", icon='X').index = i
 
-# Define custom properties for objects
-def register_custom_properties():
-	# EnumProperty to select the active property group
-	bpy.types.Object.selected_property_group = bpy.props.EnumProperty(
-		name="Stardis Object Type",
-		description="Stardis Object Type",
-		items=property_items,
-		default='SOLID'
-	)
+			# Display the properties based on the type
+			if prop.stardis_object_type == 'SOLID':
+				solid = prop.solid
 
-	# FloatProperty for conductivity
-	bpy.types.Object.conductivity = bpy.props.FloatProperty(
+				row = panel.row()
+				row.prop(solid, "conductivity")
+				row = panel.row()
+				row.prop(solid, "rho")
+				row = panel.row()
+				row.prop(solid, "capacity")
+
+				row = panel.row()
+				col = row.split()
+				col.prop(solid, "delta")
+				col.prop(solid, "delta_auto", text="Auto")
+
+				row = panel.row()
+				col = row.split()
+				col.prop(solid, "imposed_temp")
+				col.prop(solid, "imposed_temp_unknown", text="Unknown")
+
+				row = panel.row()
+				row.prop(solid, "initial_temp")
+				row = panel.row()
+				row.prop(solid, "volumic_power")
+				col = panel.column()
+				col.props_enum(solid, "triangle_sides")
+			
+			elif prop.stardis_object_type == 'DIRICHLET':
+				dirichlet = prop.dirichlet
+
+				row = panel.row()
+				row.prop(dirichlet, "temp")
+
+			elif prop.stardis_object_type in ['ROBIN_SOLID', 'ROBIN_FLUID']:
+				robin = prop.robin
+				row = panel.row()
+				row.prop(robin, "emissivity")
+				row = panel.row()
+				row.prop(robin, "specular_fraction")
+				row = panel.row()
+				row.prop(robin, "hc")
+				row = panel.row()
+				row.prop(robin, "outside_temp")
+			
+			else:
+				row = panel.row()
+				row.label(text="No properties available for this type")
+
+
+class StardisSolidProperty(bpy.types.PropertyGroup):
+	conductivity: bpy.props.FloatProperty(
 		name="Conductivity",
 		description="Conductivity",
 		default=1.0,
 		min=0.0
 	)
 
-	bpy.types.Object.rho = bpy.props.FloatProperty(
+	rho: bpy.props.FloatProperty(
 		name="Rho",
 		description="Rho",
 		default=1.0,
 		min=0.0
 	)
 
-	bpy.types.Object.capacity = bpy.props.FloatProperty(
+	capacity: bpy.props.FloatProperty(
 		name="Capacity",
 		description="Capacity",
 		default=1.0,
 		min=0.0
 	)
 
-	# delta can be a float or a string "AUTO"
-	bpy.types.Object.delta = bpy.props.FloatProperty(
+	delta_auto: bpy.props.BoolProperty(
+		name="Delta Auto",
+		description="Delta Auto",
+		default=True
+	)
+
+	delta: bpy.props.FloatProperty(
 		name="Delta",
 		description="Delta",
 		default=1.0,
 		min=0.0
 	)
 
-	bpy.types.Object.initial_temp = bpy.props.FloatProperty(
-		name="Initial Temp",
-		description="Initial Temp",
-		default=1.0,
-		min=0.0
-	)
-
-	bpy.types.Object.imposed_temp = bpy.props.FloatProperty(
+	imposed_temp: bpy.props.FloatProperty(
 		name="Imposed Temp",
 		description="Imposed Temp",
-		default=1.0,
+		default=300.0,
 		min=0.0
 	)
 
-	bpy.types.Object.volumic_power = bpy.props.FloatProperty(
+	imposed_temp_unknown: bpy.props.BoolProperty(
+		name="Imposed Temp Unknown",
+		description="Imposed Temp Unknown",
+		default=True,
+	)
+
+	initial_temp: bpy.props.FloatProperty(
+		name="Initial Temp",
+		description="Initial Temp",
+		default=300.0,
+		min=0.0
+	)
+
+	volumic_power: bpy.props.FloatProperty(
 		name="Volumic Power",
 		description="Volumic Power",
 		default=1.0,
 		min=0.0
 	)
 
-	bpy.types.Object.temp = bpy.props.FloatProperty(
+	triangle_sides: bpy.props.EnumProperty(
+		name="Triangle Sides",
+		description="Triangle Sides",
+		items=[
+			('FRONT', "Front", ""),
+			('BACK', "Back", ""),
+			('BOTH', "Both", ""),
+		],
+		default='BOTH',
+	)
+
+class StardisDirichletProperty(bpy.types.PropertyGroup):
+	temp: bpy.props.FloatProperty(
 		name="Temperature",
 		description="Temperature",
 		default=300.0,
 		min=0.0
 	)
 
+class StardisRobinSolidProperty(bpy.types.PropertyGroup):	
+	emissivity: bpy.props.FloatProperty(
+		name="Emissivity",
+		description="Emissivity",
+		default=1.0,
+		min=0.0,
+		max=1.0
+	)
+
+	specular_fraction: bpy.props.FloatProperty(
+		name="Specular Fraction",
+		description="Specular Fraction",
+		default=1.0,
+		min=0.0,
+		max=1.0
+	)
+
+	hc: bpy.props.FloatProperty(
+		name="hc",
+		description="Convective Heat Transfer Coefficient",
+		default=1.0,
+		min=0.0
+	)
+
+	outside_temp: bpy.props.FloatProperty(
+		name="Outside Temp",
+		description="Outside Temp",
+		default=300.0,
+		min=0.0
+	)
+
+
+class StardisRobinFluidProperty(bpy.types.PropertyGroup):
+	pass
+
+
+
+class StardisObjectProperty(bpy.types.PropertyGroup):
+	stardis_object_type: bpy.props.EnumProperty(
+		name="Stardis Object Type",
+		description="Stardis Object Type",
+		items=property_items,
+		default='SOLID'
+	)
+
+	solid: bpy.props.PointerProperty(type=StardisSolidProperty)
+	dirichlet: bpy.props.PointerProperty(type=StardisDirichletProperty)
+	robin_solid: bpy.props.PointerProperty(type=StardisRobinSolidProperty)
+	robin_fluid: bpy.props.PointerProperty(type=StardisRobinFluidProperty)
+
+
+# Define custom properties for objects
+def register_custom_properties():
+	bpy.utils.register_class(StardisSolidProperty)
+	bpy.utils.register_class(StardisDirichletProperty)
+	bpy.utils.register_class(StardisRobinSolidProperty)
+	bpy.utils.register_class(StardisRobinFluidProperty)
+
+	bpy.utils.register_class(StardisObjectProperty)
+
+	bpy.types.Object.stardis_object_properties = bpy.props.CollectionProperty(type=StardisObjectProperty)
+
+
 # Register and Unregister classes and properties
 def register_custom_properties_panel():
 	register_custom_properties()
-	bpy.utils.register_class(OBJECT_PT_main_panel)
+	bpy.utils.register_class(StardisObjectPropertyPanel)
 
 def unregister_custom_properties_panel():
-	bpy.utils.unregister_class(OBJECT_PT_main_panel)
-	del bpy.types.Object.my_custom_color
-	del bpy.types.Object.my_custom_shininess
-	del bpy.types.Object.my_custom_transparency
-	del bpy.types.Object.selected_property_group
+	bpy.utils.unregister_class(StardisObjectProperty)
+	bpy.utils.unregister_class(StardisSolidProperty)
+	bpy.utils.unregister_class(StardisDirichletProperty)
+	bpy.utils.unregister_class(StardisRobinSolidProperty)
+	bpy.utils.unregister_class(StardisRobinFluidProperty)
+
+	del bpy.types.Object.stardis_object_properties
+
+	bpy.utils.unregister_class(StardisObjectPropertyPanel)
+
 
 
 # Add the export option to the export menu
@@ -198,11 +342,17 @@ def register():
 	bpy.utils.register_class(ExportCustomFormatOperator)
 	bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
+	bpy.utils.register_class(AddStardisObjectProperty)
+	bpy.utils.register_class(RemoveStardisObjectProperty)
+
 	register_custom_properties_panel()
 
 def unregister():
 	bpy.utils.unregister_class(ExportCustomFormatOperator)
 	bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
+
+	bpy.utils.unregister_class(AddStardisObjectProperty)
+	bpy.utils.unregister_class(RemoveStardisObjectProperty)
 	
 	unregister_custom_properties_panel()
 
